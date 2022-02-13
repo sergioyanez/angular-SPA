4f586a9c3b34048eec3aee5e436ea12a421101c1
sergioyanez <sergiomyanez02@gmail.com>
2022-02-13 13:20:35 -0300

    Se corrige error

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HeroesService } from '../..//servicios/heroes.service';
+import { HeroesService } from '../../servicios/heroes.service';
 import { Router } from '@angular/router';
 
 @Component({
